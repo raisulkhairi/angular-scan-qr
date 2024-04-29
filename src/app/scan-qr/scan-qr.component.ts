a7c354d14cce36c90dc578fba046d1128b3441cb
raisul khairi <83000114+raisulkhairi@users.noreply.github.com>
2024-04-29 14:46:17 +0700

    fixing styling

--- src/app/scan-qr/scan-qr.component.ts
+++ src/app/scan-qr/scan-qr.component.ts
@@ -14,19 +14,7 @@ export class ScanQrComponent implements AfterViewInit {
   contraint = {
     audio: false,
     video: {
-      width: {
-        min: 1280,
-        ideal: 1920,
-        max: 2560,
-      },
-      height: {
-        min: 720,
-        ideal: 1080,
-        max: 1440
-      },
-      facingMode: {
-        exact: 'environment'
-      }
+      facingMode: 'user'
     }
   }
 
